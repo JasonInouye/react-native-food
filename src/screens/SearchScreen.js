c6d99d8426b3a8e144796b4fa2ee5cb8c65db4b9
Jason Inouye <jasonjinouye@gmail.com>
2023-09-21 15:27:28 -0500

    Result List Details withNavigation refactor

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -24,9 +24,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <ScrollView>
-        <ResultsList results={filterResultByPrice('$')} title="Cost Effective" />
-        <ResultsList results={filterResultByPrice('$$')} title="Bit Pricier" />
-        <ResultsList results={filterResultByPrice('$$$')} title="Big Spender" />
+        <ResultsList results={filterResultByPrice('$')} title="Cost Effective"/>
+        <ResultsList results={filterResultByPrice('$$')} title="Bit Pricier"/>
+        <ResultsList results={filterResultByPrice('$$$')} title="Big Spender"/>
       </ScrollView>
     </>
   )
